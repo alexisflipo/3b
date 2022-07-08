2e3740400a65fad1f112530b564cb25550a4176e
alexisflipo <alexis.flipo1@gmail.com>
2022-07-08 21:28:18 +0200

    Website

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 import os
 application = Flask(__name__)
 # application.config["MYSQL_URI"] = 'mysql://' + os.environ['MYSQL_USER'] + ':' + os.environ['MYSQL_PASSWORD'] + '@' + os.environ['MYSQL_HOSTNAME'] + ':3306/' + os.environ['MYSQL_DB']
@@ -7,9 +7,9 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    return "hello"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
     ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
-    application.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
+    application.run(host='0.0.0.0', port=ENVIRONMENT_PORT)
