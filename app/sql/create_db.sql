ea6b0633adf509a705221501a4955e4e937fa560
alexisflipo <alexis.flipo1@gmail.com>
2022-07-21 11:14:47 +0200

    Test copy

--- app/sql/create_db.sql
+++ app/sql/create_db.sql
@@ -1,20 +1,21 @@
 -- final_project.Users definition
+USE final_project;
 
--- CREATE TABLE users (
--- 	user_id INTEGER auto_increment NOT NULL,
--- 	email varchar(100) NOT NULL,
--- 	first_name varchar(100) NOT NULL,
--- 	last_name varchar(100) NOT NULL,
--- 	password varchar(100) NOT NULL,
--- 	CONSTRAINT Users_PK PRIMARY KEY (user_id)
--- )
--- ENGINE=InnoDB
--- DEFAULT CHARSET=utf8
--- COLLATE=utf8_general_ci;
--- DROP TABLE IF EXISTS Users;
+CREATE TABLE users (
+	user_id INTEGER auto_increment NOT NULL,
+	email varchar(100) NOT NULL,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	password varchar(100) NOT NULL,
+	CONSTRAINT Users_PK PRIMARY KEY (user_id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+DROP TABLE IF EXISTS Users;
 
 -- `3b`.articles definition
-USE final_project;
+
 
 CREATE TABLE articles (
   description text NOT NULL,
